5c0832d38bb66d24f939ae7904084197b0f46450
30abhishek <68611690+30abhishek@users.noreply.github.com>
2022-02-21 10:58:39 +0530

    Add files via upload

---
+++ assessment/Q1.java
@@ -0,0 +1,33 @@
+package com.company.assessment;
+
+import java.util.Stack;
+
+public class Q1{
+    public static void main(String[] args) {
+        System.out.println(isValid("()".toCharArray()));
+        System.out.println(isValid("(){}[]".toCharArray()));
+        System.out.println(isValid("([".toCharArray()));
+        System.out.println(isValid("((())".toCharArray()));
+
+    }
+
+    public static boolean isValid(char[] charArray){
+        Stack<Character> container = new Stack<Character>();
+        for(char c : charArray){
+            if(c==' '){
+                continue;
+            }
+            if (c== '(' || c=='{' || c == '['){
+                container.push(c);
+            }
+            else if ((c == ')' && container.peek() == '(') || (c == '}' && container.peek() == '{') || (c == ']' && container.peek() == '[')){
+                container.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        return container.isEmpty();
+    }
+}
+
