a578504031bde7da259366dccfc92e05ec0d8d5f
30abhishek <68611690+30abhishek@users.noreply.github.com>
2022-02-21 12:33:42 +0530

    Update Question4.java

--- assessment/Question4.java
+++ assessment/Question4.java
@@ -9,9 +9,9 @@ public class Question4 {
                 { 3, 2, 4 },
                 { 5, 4, 1 } };
         System.out.println(a.length-1);
-        System.out.println(checkmatrinx(a,a.length-1));
+        System.out.println(matrix(a,a.length-1));
     }
-    public static boolean checkmatrinx(int[][] a,int N){
+    public static boolean matrix(int[][] a,int N){
         int transpose[][] = new int[f][l];
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
