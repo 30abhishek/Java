bd1a6d9a82165de8e91b5b7ff402307c40e243ec
30abhishek <68611690+30abhishek@users.noreply.github.com>
2022-02-21 13:10:55 +0530

    Add files via upload

---
+++ assessment/Question8.java
@@ -0,0 +1,20 @@
+package com.company.assessment;
+
+import java.util.Arrays;
+
+public class Question8 {
+    public static void main(String[] args) {
+        System.out.println(Anagram("123","321"));
+    }
+
+    static boolean Anagram(String str1,String str2){
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
